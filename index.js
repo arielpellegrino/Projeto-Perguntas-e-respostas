a15acc807fdb85242e58d4753b8cde7f9623315b
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-02-19 13:36:56 -0300

    feat: bootstrap menu

--- index.js
+++ index.js
@@ -2,31 +2,21 @@ const express = require("express");
 const app = express();
 const porta = 8080;
 
+
 //Estou dizendo ao express usar o EJS como engine
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
+app.get("/", (req, res) => {
+    res.render("index");
+})
 
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "carne", preco: 5},
-        {nome: "redbull", preco: 6}
-    ];
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
 
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.post("/salvarpergunta", (req, res) => {
+    res.send('Formulário recebido')
 })
 
 app.listen(porta, () => {
